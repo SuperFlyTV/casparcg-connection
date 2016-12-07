4233204cf39fcea1762d6464e3aa2c7a2c41e290
Jesper Stærkær <jesper@starkar.no>
2016-12-07 15:43:54 +0100

    added wrapping of single devices (bluefish and decklink) in array structure. needs testing

--- src/lib/ResponseParsers.ts
+++ src/lib/ResponseParsers.ts
@@ -514,6 +514,18 @@ export namespace Response {
 		 * 
 		 */
 		public parse(data: Object): Object {
+			
+			// wrap devices in arrays (if single device of a type)
+			if(data.hasOwnProperty("decklink") && data["decklink"].hasOwnProperty("device")) {
+				if(!Array.isArray(data["decklink"]["device"])) {
+					data["decklink"]["device"] = [data["decklink"]["device"]];
+				}
+			}
+			if(data.hasOwnProperty("bluefish") && data["bluefish"].hasOwnProperty("device")) {
+				if(!Array.isArray(data["bluefish"]["device"])) {
+					data["bluefish"]["device"] = [data["bluefish"]["device"]];
+				}
+			}
 			return data;
 		}
 	}
