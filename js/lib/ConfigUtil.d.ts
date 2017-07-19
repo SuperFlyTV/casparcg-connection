07dcb6b1a820b26773c074cf07004cfce2a96ce8
Jesper Stærkær <jesper@starkar.no>
2017-07-19 11:54:30 +0200

    chore(Workflow): Try to remove js/ docs/ to smooth out the workflow with npm release script

--- js/lib/ConfigUtil.d.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from "./Config";
-/***/
-export declare namespace ConfigUtil {
-    /***/
-    function parseConfigFrom207XML(XMLString: string): Config.Intermediate.CasparCGConfig | {};
-    /***/
-    function parseConfigFrom210XML(XMLString: string): Config.Intermediate.CasparCGConfig | {};
-}
