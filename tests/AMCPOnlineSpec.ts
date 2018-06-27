308d441ebadf47a5cfc34e1401a092d930b3d1c4
Jesper Stærkær <jesper@starkar.no>
2018-06-27 18:04:41 +0200

    chore: removes jasmine

--- tests/AMCPOnlineSpec.ts
+++ tests/AMCPOnlineSpec.ts
@@ -1,24 +1,24 @@
-import "jasmine";
-import {CasparCG} from "../src/CasparCG";
-import {Enum} from "../src/lib/ServerStateEnum";
+// import "jasmine";
+// import {CasparCG} from "../src/CasparCG";
+// import {Enum} from "../src/lib/ServerStateEnum";
 
-describe("AMCPProtocol_v2_1", () => {
-	describe("basic signature", () => {
+// describe("AMCPProtocol_v2_1", () => {
+// 	describe("basic signature", () => {
 
-		beforeAll((done) => {
-			this.instance = new CasparCG({host: "52.208.248.56", onConnected: () => done()});
-		});
+// 		beforeAll((done) => {
+// 			this.instance = new CasparCG({host: "52.208.248.56", onConnected: () => done()});
+// 		});
 
-		afterAll(() => {
-			this.instance.disconnect();
-		});
+// 		afterAll(() => {
+// 			this.instance.disconnect();
+// 		});
 
-		it("version", () => {
-			this.response = this.instance.version();
-		});
+// 		it("version", () => {
+// 			this.response = this.instance.version();
+// 		});
 
-		it("helpConsumer", () => {
-			this.response = this.instance.helpConsumer(Enum.Consumer.SCREEN);
-		});
-	});
-});
+// 		it("helpConsumer", () => {
+// 			this.response = this.instance.helpConsumer(Enum.Consumer.SCREEN);
+// 		});
+// 	});
+// });
