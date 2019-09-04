687fea707ccf07fd43af3d46faac54e25fde36cc
Richard Cartwright <spark@streampunk.media>
2019-09-04 12:34:27 +0100

    chore: basic CasparCG AMCP mock

--- src/__tests__/mock_caspar.ts
+++ src/__tests__/mock_caspar.ts
@@ -17,7 +17,6 @@ server.on('listening', () => {
 })
 
 server.on('connection', sock => {
-	sock.write('Its hell from him')
 	let chunk = ''
 	sock.on('data', input => {
 		chunk += input.toString()
@@ -31,7 +30,7 @@ server.on('connection', sock => {
 				break
 			}
 			sock.write(result.toString() + '\r\n')
-			console.log(result)
+			// console.log(result)
 			if (result === '202 KILL OK') {
 				sock.destroy()
 				break
@@ -53,7 +52,25 @@ function processCommand(command: string[] | null, token = ''): string {
 		return '***BYE***'
 	}
 	if (responses218[command[0]]) {
-		return token ? `REQ ${token} ${responses218[command[0]](command)}` : responses218[command[0]](command)
+		let responseFn = responses218[command[0]]
+		let response: string | null = null
+		if (typeof responseFn === 'function') {
+			response = responseFn(command)
+		} else {
+			if (responseFn.none && command.length === 1) {
+				response = (responseFn.none as (req: string[]) => string | null)(command)
+			}	else if (responseFn.number && command.length >= 2) {
+				response = (responseFn.number as (req: string[]) => string | null)(command)
+			} else if (responseFn.layer && command.length >= 3) {
+				response = ((responseFn.layer as Responses)[command[2]] as (req: string[]) => string | null)(command)
+			} else if (command.length >= 2 && responseFn[command[1]]) {
+				response = (responseFn[command[1]] as (req: string[]) => string | null)(command)
+			}
+			if (response == null && responseFn.string && command.length >= 2) {
+				response = (responseFn.string as (req: string[]) => string | null)(command)
+			}
+		}
+		if (response) return token ? `REQ ${token} ${response}` : response
 	}
 	return token ? `REQ ${token} 400 ERROR` : '400 ERROR'
 }
@@ -89,39 +106,43 @@ let responses218: Responses = {
 		REMOVE: () => 'DATA REMOVE'
 	},
 	CG: {
-		ADD: () => 'CG ADD',
-		PLAY: () => 'CG PLAY',
-		STOP: () => 'CG STOP',
-		NEXT: () => 'CG NEXT',
-		REMOVE: () => 'CG REMOVE',
-		CLEAR: () => 'CG CLEAR',
-		UPDATE: () => 'CG UPDATE',
-		INVOKE: () => 'CG INVOKE',
-		INFO: () => 'CG INFO'
+		layer: {
+			ADD: () => 'CG ADD',
+			PLAY: () => 'CG PLAY',
+			STOP: () => 'CG STOP',
+			NEXT: () => 'CG NEXT',
+			REMOVE: () => 'CG REMOVE',
+			CLEAR: () => 'CG CLEAR',
+			UPDATE: () => 'CG UPDATE',
+			INVOKE: () => 'CG INVOKE',
+			INFO: () => 'CG INFO'
+		}
 	},
 	MIXER: {
-		KEYER: () => 'MIXER KEYER',
-		CHROMA: () => 'MIXER CHROMA',
-		BLEND: () => 'MIXER BLEND',
-		INVERT: () => 'MIXER_INVERT',
-		OPACITY: () => 'MIXER OPACITY',
-		BRIGHTNESS: () => 'MIXER BRIGHTNESS',
-		SATURATION: () => 'MIXER SATURATION',
-		CONTRAST: () => 'MIXER CONTRAST',
-		LEVELS: () => 'MIXER LEVELS',
-		FILL: () => 'MIXER FILL',
-		CLIP: () => 'MIXER CLIP',
-		ANCHOR: () => 'MIXER ANCHOR',
-		CROP: () => 'MIXER CROP',
-		ROTATION: () => 'MIXER ROTATION',
-		PERSPECTIVE: () => 'MIXER PERSPECTIVE',
-		MIPMAP: () => 'MIXER MIPMAP',
-		VOLUME: () => 'MIXER VOLUME',
-		MASTERVOLUME: () => 'MIXER MASTERVOLUME',
-		STRAIGHT_ALPHA_OUTPUT: () => 'MIXER STRAIGHT_ALPHA_OUTPUT',
-		GRID: () => 'MIXER GRID',
-		COMMIT: () => 'MIXER COMMIT',
-		CLEAR: () => 'MIXER CLEAR'
+		layer: {
+			KEYER: () => 'MIXER KEYER',
+			CHROMA: () => 'MIXER CHROMA',
+			BLEND: () => 'MIXER BLEND',
+			INVERT: () => 'MIXER_INVERT',
+			OPACITY: () => 'MIXER OPACITY',
+			BRIGHTNESS: () => 'MIXER BRIGHTNESS',
+			SATURATION: () => 'MIXER SATURATION',
+			CONTRAST: () => 'MIXER CONTRAST',
+			LEVELS: () => 'MIXER LEVELS',
+			FILL: () => 'MIXER FILL',
+			CLIP: () => 'MIXER CLIP',
+			ANCHOR: () => 'MIXER ANCHOR',
+			CROP: () => 'MIXER CROP',
+			ROTATION: () => 'MIXER ROTATION',
+			PERSPECTIVE: () => 'MIXER PERSPECTIVE',
+			MIPMAP: () => 'MIXER MIPMAP',
+			VOLUME: () => 'MIXER VOLUME',
+			MASTERVOLUME: () => 'MIXER MASTERVOLUME',
+			STRAIGHT_ALPHA_OUTPUT: () => 'MIXER STRAIGHT_ALPHA_OUTPUT',
+			GRID: () => 'MIXER GRID',
+			COMMIT: () => 'MIXER COMMIT',
+			CLEAR: () => 'MIXER CLEAR'
+		}
 	},
 	CHANNEL_GRID: () => '202 CHANNEL_GRID OK',
 	THUMBNAIL: {
@@ -134,32 +155,88 @@ let responses218: Responses = {
 	CLS: () => 'CLS',
 	FLS: () => 'FLS',
 	TLS: () => 'TLS',
-	VERSION: () => 'VERSION',
-	INFO: () => 'INFO',
-	INFO_TEMPLATE: () => 'INFO TEMPLATE',
-	INFO_CONFIG: () => 'INFO CONFIG',
-	INFO_PATHS: () => 'INFO PATHS',
-	INFO_SYSTEM: () => 'INFO SYSTEM',
-	INFO_SERVER: () => 'INFO SERVER',
-	INFO_QUEUES: () => 'INFO QUEUES',
-	INFO_THREADS: () => 'INFO THREADS',
-	INFO_DELAY: () => 'INFO DELAY',
+	VERSION: () => '201 VERSION OK\r\n2.1.8.12205 62ea2b24d NRK',
+	INFO: {
+		none: () => 'INFO',
+		number: () => 'INFO channel',
+		TEMPLATE: () => 'INFO TEMPLATE',
+		CONFIG: () => 'INFO CONFIG',
+		PATHS: () => 'INFO PATHS',
+		SYSTEM: () => 'INFO SYSTEM',
+		SERVER: () => 'INFO SERVER',
+		THREADS: () => 'INFO THREADS',
+		DELAY: () => 'INFO DELAY'
+	},
 	DIAG: () => '202 DIAG OK',
-	GL_INFO: () => 'GL INFO',
-	GL_GC: () => 'GL GC',
 	// BYE: () => 'BYE',
 	KILL: () => '202 KILL OK',
 	RESTART: () => '202 RESTART OK',
 	PING: () => 'PONG',
-	HELP: () => 'HELP',
-	HELP_PRODUCER: () => 'HELP PRODUCER',
-	HELP_CONSUMER: () => 'HELP CONSUMER',
+	HELP: {
+		none: () => 'HELP', // commands
+		string: () => 'HELP command',
+		PRODUCER: () => 'HELP PRODUCER',
+		CONSUMER: () => 'HELP CONSUMER'
+	},
 	TIME: () => 'TIME',
-	SCHEDULE_SET: () => 'SCHEDULE_SET',
-	SCHEDULE_LIST: () => 'SCHEDULE_LIST',
-	SCHEDULE_CLEAR: () => 'SCHEDULE_CLEAR',
-	SCHEDULE_REMOVE: () => 'SCHEDULE_REMOVE',
-	SCHEDULE_INFO: () => 'SCHEDULE_INFO',
-	TIMECODE_SOURCE: () => 'TIMECODE_SOURCE'
-	// TODO Command list?
+	SCHEDULE: {
+		SET: () => 'SCHEDULE_SET',
+		LIST: () => 'SCHEDULE_LIST',
+		CLEAR: () => 'SCHEDULE_CLEAR',
+		REMOVE: () => 'SCHEDULE_REMOVE',
+		INFO: () => 'SCHEDULE_INFO'
+	},
+	TIMECODE: {
+		layer: {
+			SOURCE: () => 'TIMECODE_SOURCE'
+		}
+	}
 }
+
+let responses207: Responses = Object.assign({}, responses218, {
+	VERSION: () => '201 VERSION OK\r\n2.0.7.e9fc25a Stable',
+	ROUTE: () => 'ROUTE',
+	GL_INFO: () => 'GL INFO',
+	GL_GC: () => 'GL GC'
+})
+
+responses207.LOG = Object.assign({}, responses218.LOG)
+delete (responses207.LOG as Responses).CATEGORY
+let mixerLayer = Object.assign({}, (responses218.MIXER as Responses).layer)
+delete (mixerLayer as Responses).INVERT
+responses207.MIXER = Object.assign({}, { layer: mixerLayer })
+delete responses207.FLS
+delete responses207.HELP
+delete responses207.TIME
+delete responses207.PING
+delete responses207.SCHEDULE
+delete responses207.TIMECODE
+
+let info = Object.assign({}, responses218.INFO as Responses)
+info.QUEUES = () => 'INFO QUEUES'
+responses207.INFO = info
+
+let responses220: Responses = Object.assign({}, responses218, {
+	VERSION: () => '201 VERSION OK\r\n2.2.0 66a9e3e2 Stable'
+})
+
+responses220.LOG = Object.assign({}, responses218.LOG)
+delete (responses220.LOG as Responses).CATEGORY
+let cgLayer = Object.assign({}, (responses218.CG as Responses).layer)
+delete (cgLayer as Responses).INFO
+responses220.CG = Object.assign({}, { layer: cgLayer })
+delete ((responses220.CG as Responses).layer as Responses).INFO
+mixerLayer = Object.assign({}, (responses218.MIXER as Responses).layer)
+delete (mixerLayer as Responses).INVERT
+delete (mixerLayer as Responses).STRAIGHT_ALPHA_OUTPUT
+responses220.MIXER = Object.assign({}, { layer: mixerLayer })
+responses220.INFO = {
+	none: (responses218.INFO as Responses).none,
+	number: (responses218.INFO as Responses).number
+}
+delete responses220.HELP
+delete responses220.TIME
+delete responses220.SCHEDULE
+delete responses220.TIMECODE
+
+console.log(responses207)
